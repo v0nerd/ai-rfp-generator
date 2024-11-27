1b95785aa1de4eae041bf2bdcba39bd466d12ce8
v0nerd <vontariotaylor36@gmail.com>
2024-11-27 15:01:39 -0600

    fix(port): fixed main.py port

--- main.py
+++ main.py
@@ -198,6 +198,6 @@ if __name__ == "__main__":
     uvicorn.run(
         app,
         host="0.0.0.0",
-        port=int(os.getenv("PORT", 8080)),
+        port=8080,
         proxy_headers=True,
     )
