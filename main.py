c245e55ed87c1482d708a672d4bc3b953debd187
v0nerd <vontariotaylor36@gmail.com>
2024-11-27 14:56:35 -0600

    feat(struture): restructure main.py for utilization

--- main.py
+++ main.py
@@ -199,7 +199,5 @@ if __name__ == "__main__":
         app,
         host="0.0.0.0",
         port=int(os.getenv("PORT", 8080)),
-        debug=os.getenv("DEBUG", False),
-        log_level=os.getenv("LOG_LEVEL", "info"),
         proxy_headers=True,
     )
